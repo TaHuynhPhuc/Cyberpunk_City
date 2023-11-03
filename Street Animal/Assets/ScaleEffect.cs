0473a174a711815f74170b30c7b45be23787a775
Tạ Huỳnh Phúc <102749112+TaHuynhPhuc@users.noreply.github.com>
2023-11-03 22:55:38 +0700

    3/11/2023
    Create button game

---
+++ Street Animal/Assets/ScaleEffect.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ScaleEffect : MonoBehaviour
+{
+    public float scaleFactor = 1.5f; // Tỷ lệ tăng kích thước
+    public float duration = 0.5f; // Thời gian để thực hiện hiệu ứng
+
+    private Vector3 originalScale; // Kích thước gốc
+
+    void Start()
+    {
+        originalScale = transform.localScale; // Lưu kích thước gốc
+    }
+
+    public void Enlarge()
+    {
+        StartCoroutine(ScaleOverTime(scaleFactor, duration)); // Gọi hàm ScaleOverTime với tỷ lệ tăng kích thước
+    }
+
+    public void Shrink()
+    {
+        StartCoroutine(ScaleOverTime(1.0f, duration)); // Gọi hàm ScaleOverTime với tỷ lệ gốc (1.0)
+    }
+
+    private IEnumerator ScaleOverTime(float targetScale, float time)
+    {
+        float elapsedTime = 0;
+        Vector3 targetSize = originalScale * targetScale;
+
+        while (elapsedTime < time)
+        {
+            transform.localScale = Vector3.Lerp(originalScale, targetSize, (elapsedTime / time));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localScale = targetSize; // Đảm bảo kích thước cuối cùng là kích thước mục tiêu
+    }
+}
---
+++ Street Animal/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour
+{
+    [SerializeField] private GameObject menuPause;
+    [SerializeField] private GameObject pauseButton;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseButton();
+        }
+    }
+
+    public void PauseButton()
+    {
+        Time.timeScale = 0;
+        pauseButton.SetActive(false);
+        menuPause.SetActive(true);
+    }
+
+    public void ContinueButton()
+    {
+        menuPause.SetActive(false);
+        pauseButton.SetActive(true);
+        Time.timeScale = 1;
+    }
+
+    public void RestartButton()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void MenuButton()
+    {
+
+    }
+}
